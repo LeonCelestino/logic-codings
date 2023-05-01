11d7d11be3b9cfe09ba057c6357e15f902b2d12a
unknown <leoncelestino@outlook.com>
2023-05-01 20:05:53 -0300

    Harry Potter money calculated with success

--- harryPotter/harryPotter.js
+++ harryPotter/harryPotter.js
@@ -1,1 +1,17 @@
- 
+// Função aceita 3 argumentos: quantidade de sicle, quantidade de nuque e quantidade de galeão, respectivamente
+
+function harryMoney(qntSicle, qntNuque, qntGal){
+    const SICLE_PER_GAL = 29;
+    const NUQUE_PER_GAL = 493;
+
+    // Converterá os sicle e nuques para galeão, somando com o galeão, e arredonda para o inteiro mais próximo
+    const totalGaleao = Math.round(qntSicle / SICLE_PER_GAL + qntNuque / NUQUE_PER_GAL + qntGal);
+  
+    return `${totalGaleao}`;
+}
+
+let harrySicles;
+let harryNuques;
+let harryGaleao;
+
+console.log(harryMoney(harrySicles, harryNuques, harryGaleao))
